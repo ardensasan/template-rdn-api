90aed9a03225c8f20a4697bf383ab002540b100c
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-12-03 08:30:50 +0800

    added create table query

--- src/model/generator.ts
+++ src/model/generator.ts
@@ -1,6 +1,10 @@
 import setConnection from "../config/dbConfig";
 const connection = setConnection();
-export const createTable = (name:string) =>{
-    console.log('%c 🌰 name: ', 'font-size:20px;background-color: #E41A6A;color:#fff;', name);
-    // return connection.promise("create table").then((data: any) => data);
-}
+export const createTable = (name: string) => {
+  // console.log('%c 🌰 name: ', 'font-size:20px;background-color: #E41A6A;color:#fff;', name);
+  return connection
+    .promise(
+      "CREATE TABLE Persons (PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255))"
+    )
+    .then((data: any) => data);
+};

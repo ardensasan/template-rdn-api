162bd3030652ee0529f44a8953d1a75a5966aa6c
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-12-03 08:17:15 +0800

    added create table route

--- src/model/generator.ts
+++ src/model/generator.ts
@@ -1,5 +1,6 @@
 import setConnection from "../config/dbConfig";
 const connection = setConnection();
-export const createTable = () =>{
-    return connection.promise("create table").then((data: any) => data);
+export const createTable = (name:string) =>{
+    console.log('%c 🌰 name: ', 'font-size:20px;background-color: #E41A6A;color:#fff;', name);
+    // return connection.promise("create table").then((data: any) => data);
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import user from "./api/user";
+import generator from "./api/generator";
 const app: Application = express();
 app.set("port", process.env.PORT || 3000);
 app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "Hello world!" });
 });
 app.use(user);
+app.use(generator)
 app.listen(app.get("port"), () => {
   console.log(`Server on http://localhost:${app.get("port")}/`);
 });
